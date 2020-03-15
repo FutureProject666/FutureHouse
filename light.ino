16da8d7e87db0a25bc34913b5638dbb5aa887ca7
FutureProject666 <62196374+FutureProject666@users.noreply.github.com>
2020-03-15 13:43:29 +0800

    Update light.ino

--- light.ino
+++ light.ino
@@ -1,13 +1,42 @@
-#include<SoftwareSerial.h>
-SoftwareSerial ArduinoUno(7,8);
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  ArduinoUno.begin(115200);
-  delay(10);
+#include <SoftwareSerial.h>
+#define LED_PIN 13
+
+SoftwareSerial fromESP(7, 8); //Rx, Tx
+
+char inChar;
+int swit = 1;
+void setup()
+{
+  Serial.begin(115200);
+  fromESP.begin(9600);
+  delay(50);
+  pinMode(LED_PIN, OUTPUT);
+  delay(50);
+  digitalWrite(LED_PIN, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void loop()
+{
+  if (fromESP.available())
+  {
+    inChar = fromESP.read();
+    Serial.print(inChar);
+  }
+  if (inChar == '0') {
+    if (swit == 1) {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("更換為關閉");
+      swit = 0;
+    }
+    Serial.println("已經為關閉");
+  }
+  if (inChar == '1') {
+    if (swit == 0) {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("更換為開啟");
+      swit = 1;
+    }
+    Serial.println("已經為開啟");
+  }
+
 }
